eb0a86ca65ef706077ca15fa08bdb3e96385dc22
unknown <Eze-jordan>
2025-06-13 02:37:46 +0100

    maj

--- rebuild.sh
+++ rebuild.sh
@@ -3,13 +3,16 @@
 # Arrêter et supprimer tous les conteneurs, réseaux, volumes créés par docker-compose
 docker compose down
 
-# Supprimer l'image de l'application si elle existe
-docker image rm _app:latest 2>/dev/null || echo "Image _app:latest non trouvée, rien à supprimer"
+# Supprimer l'image si elle existe
+docker image rm frontend:latest 2>/dev/null || echo "Image frontend:latest non trouvée, rien à supprimer"
 
 # Récupérer les dernières modifications depuis Git
 git pull || { echo "Erreur lors du git pull"; exit 1; }
 
-# Reconstruire et relancer le service app
-docker compose up -d --build app || { echo "Erreur lors du démarrage du service"; exit 1; }
+# Reconstruire et relancer le service
+docker compose up -d --build frontend || { echo "Erreur lors du démarrage du service"; exit 1; }
 
-echo "Déploiement terminé avec succès"
+echo "✅ Déploiement terminé avec succès"
+
+
+
